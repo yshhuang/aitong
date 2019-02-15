8faadbb10665253eccc022e4f86161962fdc9c06
huangyongsheng <yshhuang@foxmail.com>
2019-02-15 15:36:03 +0800

    source code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArrayType } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'aitong';
+  imgsNumber = [7, 8, 9, 10];
+
+  ngOnInit() {
+  }
 }
