3a6b4b42fd0625a44ad460fb56398848e16a30f6
huangyongsheng <huangyongsheng@kkworld.com>
2019-09-25 16:31:05 +0800

    countdown

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+$(function () {
+    'use strict';
+    var note = $('#note'),
+        ts = new Date(2020, 12, 21),
+        newYear = true;
+
+
+	$('#countdown').countdown({
+		timestamp: ts,
+	callback: function (days, hours, minutes, seconds) {
+            var message = "";
+			message += days + " day" + (days == 1 ? '' : 's') + ", ";
+			message += hours + " hour" + (hours == 1 ? '' : 's') + ", ";
+			message += minutes + " minute" + (minutes == 1 ? '' : 's') + " and ";
+			message += seconds + " second" + (seconds == 1 ? '' : 's') + " <br />";
+
+			if (newYear) {
+				message += "left until 2020/12/21!";
+			}
+			else {
+				message += "left to 10 days from now!";
+			}
+			note.html(message);
+		}
+	});
+
+});
